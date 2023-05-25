a3ad3b32ddfa19b34f3896921719650b064d2dc8
choiyounji <100260416+choiyounji@users.noreply.github.com>
2023-05-25 21:21:07 +0900

    Update rasp.py

--- rasp.py
+++ rasp.py
@@ -17,7 +17,8 @@ def upload_to_s3(bucket_name, file_path):
 
 def record_audio(duration, num):
     for i in range(num):
-        file_name=f"record{i+1}.wav"
+        file_name=f"record1.wav"
+        #raspberry 재연결하면 마이크 포트번호 바뀔 때도 있으니 arecord -l로 확인해주고 수정해주는 단계 먼저 해줘야함!
         command=f"arecord -D plughw:3,0 -d 2 -f cd {file_name}"
         subprocess.run(command, shell=True)
         time.sleep(duration)
